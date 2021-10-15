faaf1864f746fda8d700086b3f4aa265972edad5
sumonshekh <sm.sumon117@gmail.com>
2021-10-15 11:36:09 +0600

    background scrolly repeated as player go higher and higher

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -1,9 +1,12 @@
 
 import Phaser  from "../lib/phaser.js";
-console.dir(Phaser.Scene)
+
 export default class Game extends Phaser.Scene{
+    /** @type {Phaser.Physics.Arcade.Sprite} */
+    platforms
+    player
     constructor(){
-        /** @type {Phaser.Physics.Arcade.Sprite} */
+        
         
         super('Game')
     }
@@ -15,27 +18,28 @@ export default class Game extends Phaser.Scene{
     }
 
     create(){
-        this.add.image(240,320,'background');
+        this.add.image(240,320,'background').setScrollFactor(1,0);
         //instead of this 
         //this.add.image(240,320,'platform').setScale(0.5);
         //use this
         //remove this
         //this.physics.add.image(240,320,'platform').setScale(0.5);
         
-        const platforms = this.physics.add.staticGroup();
+        this.platforms = this.physics.add.staticGroup();
         for(let i = 0; i < 5; ++i){
             const x = Phaser.Math.Between(80,400);
             const y = 150*i;
-            const platform = platforms.create(x,y,'platform');
+            const platform = this.platforms.create(x,y,'platform');
             platform.scale = 0.5;
             const body = platform.body;
             body.updateFromGameObject();
         }
          this.player = this.physics.add.sprite(240,320,'bunny-stand').setScale(0.5);
-        this.physics.add.collider(platforms, this.player);
+        this.physics.add.collider(this.platforms, this.player);
         this.player.body.checkCollision.left = false;
         this.player.body.checkCollision.right = false;
         this.player.body.checkCollision.up = false;
+        this.cameras.main.startFollow(this.player);
     }
     update(){
 
@@ -44,5 +48,18 @@ export default class Game extends Phaser.Scene{
         if(touchingDown){
             this.player.setVelocityY(-300);
         }
+        this.platforms.children.iterate(child =>{
+            const platform = child;
+            
+
+            const scrollY = this.cameras.main.scrollY;
+          
+
+            if(platform.y >= scrollY+700){
+                platform.y = scrollY- Phaser.Math.Between(50,100);
+                console.log("update platformY: " + platform.y);
+                platform.body.updateFromGameObject();
+            }
+        })
     }
 }
